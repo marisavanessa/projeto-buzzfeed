46082d208c1ffa91889641aac278bd97d72b1011
Marisa Vanessa N. Cruz <ideiaselembrancas@gmail.com>
2025-01-05 11:44:22 -0300

    Item 5 Criando as regras do jogo

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json";
 
 @Component({
   selector: 'app-quiz',
@@ -23,8 +24,36 @@ export class QuizComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false
+      this.title = quiz_questions.title
 
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+
+    }
 
   }
+  playerChoose(value:string){
+    this.answers.push(value)
+    console.log(this.answers)
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
+
 
 }
